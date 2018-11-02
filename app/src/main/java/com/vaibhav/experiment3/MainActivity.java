96bf5899f4aab1a3de8a18a65d060d71585343af
vaibhav <vaibhavagarwal032@gmail.com>
2018-11-03 00:52:34 +0530

    Initial commit

---
+++ app/src/main/java/com/vaibhav/experiment3/MainActivity.java
@@ -0,0 +1,23 @@
+package com.vaibhav.experiment3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edt;
+    String str;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edt=(EditText)findViewById(R.id.editText2);
+    }
+    public void Hye(View view) {
+        str=edt.getText().toString();
+        Toast.makeText(getApplicationContext(),str,Toast.LENGTH_SHORT).show();
+    }
+}
